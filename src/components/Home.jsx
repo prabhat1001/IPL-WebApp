261c5d61d0e750d9b03a6261a470ed6d284cb4dc
prabhat1001 <prabhatsaraswat24@gmail.com>
2022-10-01 18:39:42 +0530

    Home component

---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+import './Home.css';
+import ImgSlider from './ImgSlider';
+
+const Home = () => {
+  return (
+    <>
+    <ImgSlider/>
+    </>
+  )
+}
+
+
+export default Home
